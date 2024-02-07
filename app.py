6f487f9d50d949228ea70e9363d225dda1df31e8
Matan <matanatedgi@gmail.com>
2024-02-07 15:49:03 +0200

    Fix password length

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ migrate = Migrate(app, db)
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+    password = db.Column(db.String)
     name = db.Column(db.String(1000))
 
     review = relationship("Reviews", back_populates="author")
